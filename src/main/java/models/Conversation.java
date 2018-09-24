514f399585dbec333deeb8ec82c6425e5a65a595
Pepe Valdivia <jvaldivia@softweb.pe>
2018-09-23 21:48:15 -0500

    Creación de documentos embebidos

--- src/main/java/models/Conversation.java
+++ src/main/java/models/Conversation.java
@@ -1,9 +1,34 @@
 package models;
 
+import java.util.List;
 import org.bson.types.ObjectId;
+import org.mongodb.morphia.annotations.Embedded;
 import org.mongodb.morphia.annotations.Entity;
 import org.mongodb.morphia.annotations.Id;
 
+@Entity("conversations")
 public class Conversation {
+    @Id 
+    private ObjectId id;
+    @Embedded
+    private List<Message> messages;
 
+    public Conversation() {
+    }
+
+    public ObjectId getId() {
+        return id;
+    }
+
+    public void setId(ObjectId id) {
+        this.id = id;
+    }
+
+    public List<Message> getMessages() {
+        return messages;
+    }
+
+    public void setMessages(List<Message> messages) {
+        this.messages = messages;
+    }
 }
